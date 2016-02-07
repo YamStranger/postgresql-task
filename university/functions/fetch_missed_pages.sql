04059e05ab9a0e5ae3fca6b39e018e9d9be8d1e2
YamStranger <YamStranger@bk.ru>
2016-02-07 18:24:14 +0200

    fix: updated function interface

--- university/functions/fetch_missed_pages.sql
+++ university/functions/fetch_missed_pages.sql
@@ -1,16 +1,16 @@
 DROP FUNCTION
-IF EXISTS courses.fetch_missed_pages (BIGINT,VARCHAR);
+IF EXISTS get_missing_pages (int,text);
 
-CREATE FUNCTION courses.fetch_missed_pages(input_test_id BIGINT, version_name VARCHAR(500) DEFAULT NULL) 
-    RETURNS TABLE(student_name VARCHAR,
-									version_name_val VARCHAR,
-									indexes text)
+CREATE FUNCTION get_missing_pages(_test_id  int, _version_name text default null) 
+    RETURNS TABLE(student_name text,
+									version_name char,
+									lost_page_indices text)
    AS
 $$
 BEGIN
     RETURN QUERY
-			SELECT student_n, version_n, 
-					array_to_string(array_agg(page_index ORDER BY page_index), ' ') As indexes
+			SELECT cast(student_n as text), cast(version_n as char(500)), 
+					array_to_string(array_agg(page_index ORDER BY page_index), ' ') As indexes 
 			FROM (select 
 				stud.name as student_n, 
 				v.name as version_n, 
@@ -28,11 +28,11 @@ BEGIN
 					on stud.id=st.student_id
 				right join courses.page pg_r
 					on pg_r.version_id=v.id
-				where t.id = input_test_id
+				where t.id = _test_id
 								 AND CASE
-												WHEN version_name is not null THEN
+												WHEN _version_name is not null THEN
 													CASE
-														WHEN v.name = version_name THEN
+														WHEN v.name = _version_name THEN
 																1
 													ELSE
 																2
